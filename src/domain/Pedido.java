ad79e27b5232542ba192c10848e9674b363205e1
Thiago Neves <thiagomn03@gmail.com>
2023-08-19 20:33:21 -0300

    Atualizada a classe pedido

--- src/domain/Pedido.java
+++ src/domain/Pedido.java
@@ -1,11 +1,47 @@
 package domain;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Pedido {
-    private String decricao;
+    private String descricao;
     private LocalDateTime data;
     private boolean web;
+    private List<Produto> listaProdutos;
+    private Set<Solicitante> solicitantes;
 
-    private Produto produto[];
+    public Pedido(String descricao, LocalDateTime data, boolean web, List<Produto> listaProdutos) {
+        this.descricao = descricao;
+        this.data = data;
+        this.web = web;
+        this.listaProdutos = new ArrayList<>(listaProdutos);
+    }
+
+    public Pedido(String descricao, LocalDateTime data, boolean web, List<Produto> listaProdutos, Solicitante solicitante) {
+        this.descricao = descricao;
+        this.data = data;
+        this.web = web;
+        this.listaProdutos = new ArrayList<>(listaProdutos);
+        this.solicitantes = new HashSet<>();
+        this.solicitantes.add(solicitante);
+    }
+
+    public void adicionarProduto(Produto produto) {
+        listaProdutos.add(produto);
+    }
+
+    public void removerProduto(Produto produto) {
+        listaProdutos.remove(produto);
+    }
+
+    public void adicionarSolicitante(Solicitante solicitante) {
+        solicitantes.add(solicitante);
+    }
+
+    public void removerSolicitante(Solicitante solicitante) {
+        solicitantes.remove(solicitante);
+    }
 }
