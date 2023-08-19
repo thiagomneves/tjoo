220868a707e5e6e528d8eea0c7af2ffd44b7d52b
Thiago Neves <thiagomn03@gmail.com>
2023-08-19 19:51:25 -0300

    Implementada a classe Comida

--- src/domain/Comida.java
+++ src/domain/Comida.java
@@ -5,6 +5,53 @@ public class Comida extends Produto {
     boolean vegano;
     String ingredientes;
 
+    public Comida(String nome, float valor, int codigo) {
+        super(nome, valor, codigo);
+    }
+
+    public Comida(String nome, float valor, int codigo, float peso, boolean vegano, String ingredientes) {
+        super(nome, valor, codigo);
+        this.peso = peso;
+        this.vegano = vegano;
+        this.ingredientes = ingredientes;
+    }
+
+    @Override
+    public String toString() {
+        return "Comida{" +
+                "peso=" + peso +
+                ", vegano=" + vegano +
+                ", ingredientes='" + ingredientes + '\'' +
+                ", nome='" + nome + '\'' +
+                ", valor=" + valor +
+                ", codigo=" + codigo +
+                '}';
+    }
+
+    public float getPeso() {
+        return peso;
+    }
+
+    public void setPeso(float peso) {
+        this.peso = peso;
+    }
+
+    public boolean isVegano() {
+        return vegano;
+    }
+
+    public void setVegano(boolean vegano) {
+        this.vegano = vegano;
+    }
+
+    public String getIngredientes() {
+        return ingredientes;
+    }
+
+    public void setIngredientes(String ingredientes) {
+        this.ingredientes = ingredientes;
+    }
+
     @Override
     public float calcularImposto(float valor) {
         return 0;
