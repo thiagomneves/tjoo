b9be9e54deb8388d22a7c87944c8f33abf3a31a7
Thiago Neves <thiagomn03@gmail.com>
2023-08-19 19:52:11 -0300

    Implementada a classe Sobremesa

--- src/domain/Sobremesa.java
+++ src/domain/Sobremesa.java
@@ -5,6 +5,53 @@ public class Sobremesa extends Produto {
     boolean doce;
     String informacoes;
 
+    public Sobremesa(String nome, float valor, int codigo) {
+        super(nome, valor, codigo);
+    }
+
+    public float getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(float quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    @Override
+    public String toString() {
+        return "Sobremesa{" +
+                "quantidade=" + quantidade +
+                ", doce=" + doce +
+                ", informacoes='" + informacoes + '\'' +
+                ", nome='" + nome + '\'' +
+                ", valor=" + valor +
+                ", codigo=" + codigo +
+                '}';
+    }
+
+    public boolean isDoce() {
+        return doce;
+    }
+
+    public void setDoce(boolean doce) {
+        this.doce = doce;
+    }
+
+    public String getInformacoes() {
+        return informacoes;
+    }
+
+    public void setInformacoes(String informacoes) {
+        this.informacoes = informacoes;
+    }
+
+    public Sobremesa(String nome, float valor, int codigo, float quantidade, boolean doce, String informacoes) {
+        super(nome, valor, codigo);
+        this.quantidade = quantidade;
+        this.doce = doce;
+        this.informacoes = informacoes;
+    }
+
     @Override
     public float calcularImposto(float valor) {
         return valor * 0.2f;
