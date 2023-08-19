088cf7805f5a489b90717a925cd291eade60168d
Thiago Neves <thiagomn03@gmail.com>
2023-08-19 19:44:50 -0300

    Criado construtor com inicialização dos atributos na classe mãe

--- src/domain/Produto.java
+++ src/domain/Produto.java
@@ -5,5 +5,11 @@ public abstract class Produto {
     float valor;
     int codigo;
 
+    public Produto(String nome, float valor, int codigo) {
+        this.nome = nome;
+        this.valor = valor;
+        this.codigo = codigo;
+    }
+
     public abstract float calcularImposto(float valor);
 }
