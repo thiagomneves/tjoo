36d9112bfada0f32b514fb346ca2c86b9151e022
Thiago Neves <thiagomn03@gmail.com>
2023-08-19 19:49:18 -0300

    Implementada a classe bebida

--- src/domain/Bebida.java
+++ src/domain/Bebida.java
@@ -6,6 +6,53 @@ public class Bebida extends Produto {
     float tamanho;
     String marca;
 
+    public Bebida(String nome, float valor, int codigo) {
+        super(nome, valor, codigo);
+    }
+
+    public Bebida(String nome, float valor, int codigo, boolean gelada, float tamanho, String marca) {
+        super(nome, valor, codigo);
+        this.gelada = gelada;
+        this.tamanho = tamanho;
+        this.marca = marca;
+    }
+
+    public boolean isGelada() {
+        return gelada;
+    }
+
+    public void setGelada(boolean gelada) {
+        this.gelada = gelada;
+    }
+
+    public float getTamanho() {
+        return tamanho;
+    }
+
+    public void setTamanho(float tamanho) {
+        this.tamanho = tamanho;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    @Override
+    public String toString() {
+        return "Bebida{" +
+                "gelada=" + gelada +
+                ", tamanho=" + tamanho +
+                ", marca='" + marca + '\'' +
+                ", nome='" + nome + '\'' +
+                ", valor=" + valor +
+                ", codigo=" + codigo +
+                '}';
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
     @Override
     public float calcularImposto(float valor) {
         return valor * 0.6f;
