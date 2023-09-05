18511e88723835615ddb5807fbc7853cd5a60bb4
Thiago Neves <thiagomn03@gmail.com>
2023-09-05 20:03:05 -0300

    Alterado o código para cumprir as exigências que faltavam

--- src/main/java/util/GravadorArquivoPedido.java
+++ src/main/java/util/GravadorArquivoPedido.java
@@ -1,26 +1,36 @@
 package util;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import domain.Pedido;
-import exceptions.ArquivoIOException;
 
-import java.io.File;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class GravadorArquivoPedido {
     public static void escreverListaParaJSON(List<Pedido> pedidos, String arquivoSaida) {
-        ObjectMapper objectMapper = new ObjectMapper();
-        try {
-            for (Pedido pedido : pedidos) {
-                System.out.println("Pedido antes da escrita: " + pedido);
-            }
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(arquivoSaida))) {
+            String json = pedidos.stream()
+                    .map(pedido -> pedidoToJSON(pedido))
+                    .collect(Collectors.joining(", ", "[", "]"));
 
-            objectMapper.writeValue(new File(arquivoSaida), pedidos);
+            writer.write(json);
             System.out.println("Arquivo JSON gerado com sucesso.");
         } catch (IOException e) {
-            System.out.println("Erro ao escrever o arquivo JSON: " + e.getMessage());
+            System.err.println("Erro ao escrever o arquivo JSON: " + e.getMessage());
         }
+    }
+
+    private static String pedidoToJSON(Pedido pedido) {
+        StringBuilder jsonBuilder = new StringBuilder();
+        jsonBuilder.append("{");
+        jsonBuilder.append("\"descricao\": \"" + pedido.getDescricao() + "\", ");
+        jsonBuilder.append("\"data\": \"" + pedido.getData() + "\", ");
+        jsonBuilder.append("\"web\": " + pedido.isWeb());
+
+        jsonBuilder.append("}");
+        return jsonBuilder.toString();
     }
 
 }
